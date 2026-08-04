26bd15d6d5f85fc1b104ca796acc0b112f936585
Abolfazlyousefii <lord.abol.1383@gmail.com>
2026-08-04 19:25:43 +0330

    Stop tracking compiled Blade views

--- storage/framework/views/d0ddecd95f1ce62c2f2e85c59b5f3053.php
+++
@@ -1,5 +0,0 @@
-<link href="https://cdn.jsdelivr.net" rel="preconnect"/>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" rel="stylesheet"/>
-<link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" rel="stylesheet"/>
-<link href="<?php echo e(asset('assets/css/styles.css')); ?>" rel="stylesheet"/>
-<?php /**PATH E:\laragon\www\aliche.ir\resources\views/frontend/partials/styles.blade.php ENDPATH**/ ?>
