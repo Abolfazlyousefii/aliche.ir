c7b98eadbdc0389f54f5ad299dcc534c35c8158d
Abolfazl yousefi <150295740+Abolfazlyousefii@users.noreply.github.com>
2026-08-27 18:30:33 +0330

    Add route for union manager profile migration

--- routes/admin-maintenance.php
+++ routes/admin-maintenance.php
@@ -9,3 +9,10 @@ Route::get('maintenance/union-member-image', [MaintenanceController::class, 'uni
 Route::post('maintenance/union-member-image/run', [MaintenanceController::class, 'runUnionMemberImage'])
     ->middleware('throttle:3,1')
     ->name('union-member-image.run');
+
+Route::get('maintenance/union-manager-profile', [MaintenanceController::class, 'unionManagerProfile'])
+    ->name('union-manager-profile');
+
+Route::post('maintenance/union-manager-profile/run', [MaintenanceController::class, 'runUnionManagerProfile'])
+    ->middleware('throttle:3,1')
+    ->name('union-manager-profile.run');
